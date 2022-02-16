386132c6eeb75a1277ec02427156ec96fa716977
Caroline Hvirfvel <carolinehvirfvel@Carolines-MacBook-Pro.local>
2022-02-16 19:36:37 +0000

    all tests passing

---
+++ models/traveller.js
@@ -0,0 +1,48 @@
+const Traveller = function(journeys) {
+  this.journeys = journeys;
+};
+
+Traveller.prototype.getJourneyStartLocations = function() {
+  const startLocations = this.journeys.map((journey) => {
+    return journey.startLocation
+  })
+  return startLocations
+};
+
+Traveller.prototype.getJourneyEndLocations = function () {
+  const endLocations = this.journeys.map((journey) => {
+    return journey.endLocation
+  })
+  return endLocations
+};
+
+Traveller.prototype.getJourneysByTransport = function (transport) {
+  const journeysByTransport = this.journeys.filter((journey) => {
+    return journey.transport === transport
+  })
+  return journeysByTransport
+};
+
+Traveller.prototype.getJourneysByMinDistance = function (minDistance) {
+  const journeysOverDistance = this.journeys.filter((journey) => {
+    return journey.distance > minDistance
+  })
+  return journeysOverDistance
+};
+
+Traveller.prototype.calculateTotalDistanceTravelled = function () {
+  const totalDistance = this.journeys.reduce((runningTotal, journey) => {
+    return runningTotal + journey.distance
+  } ,0)
+  return totalDistance
+};
+
+Traveller.prototype.getUniqueModesOfTransport = function () {
+  const uniqueModesOfTransport = [...new Set(this.journeys.map((journey) => {
+    return journey.transport
+  }))]
+  return uniqueModesOfTransport
+}
+
+
+module.exports = Traveller;
